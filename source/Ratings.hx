3996b0c3b25cce275f069152086a4fd913c96bbe
YumDev12 <91219991+YumDev12@users.noreply.github.com>
2021-09-22 21:28:14 +0100

    Less Harsh Ratings Lmao

--- source/Ratings.hx
+++ source/Ratings.hx
@@ -22,22 +22,22 @@ class Ratings
         // WIFE TIME :)))) (based on Wife3)
 
         var wifeConditions:Array<Bool> = [
-            accuracy >= 99.9935, // AAAAA
-            accuracy >= 99.980, // AAAA:
-            accuracy >= 99.970, // AAAA.
-            accuracy >= 99.955, // AAAA
-            accuracy >= 99.90, // AAA:
-            accuracy >= 99.80, // AAA.
-            accuracy >= 99.70, // AAA
-            accuracy >= 99, // AA:
-            accuracy >= 96.50, // AA.
-            accuracy >= 93, // AA
+            accuracy >= 95.900, // AAAAA
+            accuracy >= 94.980, // AAAA:
+            accuracy >= 93.970, // AAAA.
+            accuracy >= 92.955, // AAAA
+            accuracy >= 91.90, // AAA:
+            accuracy >= 90.80, // AAA.
+            accuracy >= 90.70, // AAA
+            accuracy >= 90.40, // AA:
+            accuracy >= 90.50, // AA.
+            accuracy >= 90.5, // AA
             accuracy >= 90, // A:
             accuracy >= 85, // A.
             accuracy >= 80, // A
             accuracy >= 70, // B
             accuracy >= 60, // C
-            accuracy < 60 // D
+            accuracy < 50 // D
         ];
 
         for(i in 0...wifeConditions.length)
