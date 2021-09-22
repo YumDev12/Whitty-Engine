fb9394e6a74f5a48c6d95ea41fd419c06c85a0db
YumDev12 <91219991+YumDev12@users.noreply.github.com>
2021-09-22 21:08:49 +0100

    Update Story Menu

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -30,13 +30,12 @@ class StoryMenuState extends MusicBeatState
 		['Pico', 'Philly', "Blammed"],
 		['Satin-Panties', "High", "Milf"],
 		['Cocoa', 'Eggnog', 'Winter-Horrorland'],
-		['Senpai', 'Roses', 'Thorns'],
-		['Lo-Fight','Overhead', 'Ballistic'],
+		['Senpai', 'Roses', 'Thorns']
 
 	];
 	var curDifficulty:Int = 1;
 
-	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, true, true];
+	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, true];
 
 	var weekCharacters:Array<Dynamic> = [
 		['', 'bf', 'gf'],
@@ -45,8 +44,8 @@ class StoryMenuState extends MusicBeatState
 		['pico', 'bf', 'gf'],
 		['mom', 'bf', 'gf'],
 		['parents-christmas', 'bf', 'gf'],
-		['senpai', 'bf', 'gf'],
-		['whitty', 'bf', 'gf'],
+		['senpai', 'bf', 'gf']
+
 	];
 
 	var weekNames:Array<String> = [
@@ -56,8 +55,8 @@ class StoryMenuState extends MusicBeatState
 		"PICO",
 		"MOMMY MUST MURDER",
 		"RED SNOW",
-		"hating simulator ft. moawling",
-		"Back Alley Blitz",
+		"hating simulator ft. moawling"
+
 	];
 
 	var txtWeekTitle:FlxText;
